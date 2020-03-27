79df4b684b5b3e654f342ab610a9cb1f492e6a26
Louis Tran <louist@zillow.com>
2020-03-26 22:49:22 -0700

    Initial commit

---
+++ Example/MyPodName/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyPodName
+//
+//  Created by louistran99 on 03/26/2020.
+//  Copyright (c) 2020 louistran99. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
